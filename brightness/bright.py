7c927bc17d4696814533c3c44f42551f8e20f180
vohungvi <vohungvi27@gmail.com>
2022-12-18 22:03:28 +0700

    add code detect brightness

---
+++ brightness/bright.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+THRESH1 = 170
+THRESH2 = 100
+
+def CheckBright(frame):
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [2], None, [256], [0, 256])
+
+    sum = np.sum(hist)
+    dark = 0
+    bright = 0
+    for i in range(0, 65):
+        dark += hist[i, 0]
+    for i in range(230, 255):
+        bright += hist[i, 0]
+    dark_per = dark / sum
+    bright_per = bright / sum
+
+    # Compare to THRESHOLD
+    if dark_per > 0.40:
+        result = 'Dark'
+        mean = dark_per
+    elif bright_per > 0.40:
+        result = 'Bright'
+        mean = bright_per
+    else:
+        result = 'Ok'
+        mean = dark_per
+    return result, mean
